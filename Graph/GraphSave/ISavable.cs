68cb3c9d38e6e0ab7b5cce77b824ee992dfbfd45
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-02-10 14:49:32 +0100

    Upload save and tests files.

---
+++ Graph/GraphSave/ISavable.cs
@@ -0,0 +1,14 @@
+﻿using Graph.ToSerialize;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphSave
+{
+    public interface ISavable<TypeOfNodeData, TypeOfEdgeData>
+    {
+        void Save(SerializedGraphDataModel<TypeOfNodeData, TypeOfEdgeData> save);
+
+        SerializedGraphDataModel<TypeOfNodeData, TypeOfEdgeData> Load();
+    }
+}
---
+++ Graph/GraphSave/SaveToFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using Graph.ToSerialize;
+
+namespace GraphSave
+{
+    public class SaveToFile<TypeOfNodeData, TypeOfEdgeData> : ISavable<TypeOfNodeData, TypeOfEdgeData>
+    {
+        string path;
+
+        public SaveToFile(string path) =>
+            this.path = path;
+
+        public void ChangeDirectory(string path) =>
+            this.path = path;
+
+        public SerializedGraphDataModel<TypeOfNodeData, TypeOfEdgeData> Load()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            if (File.Exists(path))
+            {
+                Stream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+                return (SerializedGraphDataModel<TypeOfNodeData, TypeOfEdgeData>) formatter.Deserialize(stream);
+            }
+
+            throw new IOException("File doesn't exist in path: " + path);
+        }
+
+        public void Save(SerializedGraphDataModel<TypeOfNodeData, TypeOfEdgeData> save)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+            Stream stream = new FileStream(path, FileMode.Create, FileAccess.Write);
+
+            formatter.Serialize(stream, save);
+            stream.Close();
+        }
+    }
+}
