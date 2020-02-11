3120c8cd32f9128987b2bee274f4c1362d6973a2
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-02-11 10:45:34 +0100

    Update names.

--- Graph/GraphSave/ISavable.cs
+++ Graph/GraphSave/ISavable.cs
@@ -1,4 +1,4 @@
-﻿using Graph.ToSerialize;
+﻿using Graph.SerializableConverter;
 using System;
 using System.Collections.Generic;
 using System.Text;
--- Graph/GraphSave/SaveToFile.cs
+++ Graph/GraphSave/SaveToFile.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-using Graph.ToSerialize;
+using Graph.SerializableConverter;
 
 namespace GraphSave
 {
